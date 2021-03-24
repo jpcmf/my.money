7f2e9f161439fd2ee80de6aed940b488a17460ae
João Paulo Fricks <jpfricks@gmail.com>
2021-03-24 15:02:20 -0300

    chore: Create the table component 🍩

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from './styles';
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Value</th>
+            <th>Category</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Dev site</td>
+            <td className="deposit">R$12.0000</td>
+            <td>Freela</td>
+            <td>03/02/2021</td>
+          </tr>
+          <tr>
+            <td>Energy</td>
+            <td className="withdraw">- R$120</td>
+            <td>Home</td>
+            <td>12/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
